1af869d55451de951a0d85e1d34eff485e207540
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-06 23:09:56 +0100

    feat: :white_check_mark: post heroe route

--- src/api/routers/heroeRouter.ts
+++ src/api/routers/heroeRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { validateHeroe } from "../middlewares/heroeValidation";
 import { heroeIdSchema, heroeCreateSchema } from "../models/Joi/heroeSchemas";
-import {getHeroeById, getHeroes} from "../controllers/heroeController";
+import {createHeroe, getHeroeById, getHeroes} from "../controllers/heroeController";
 
 const heroeRouter = Router();
 
 heroeRouter.get('/', getHeroes);
 heroeRouter.get('/:id', validateHeroe(heroeIdSchema, 'params'), getHeroeById);
+heroeRouter.post('/', validateHeroe(heroeCreateSchema, 'body'), createHeroe);
 
 export default heroeRouter;
