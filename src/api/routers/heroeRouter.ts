e7274670a188820a3549bbb465fd5e2edfd4b08d
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-07 00:01:22 +0100

    feat:

--- src/api/routers/heroeRouter.ts
+++ src/api/routers/heroeRouter.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { validateHeroe } from "../middlewares/heroeValidation";
 import { heroeIdSchema, heroeCreateSchema } from "../models/Joi/heroeSchemas";
-import {createHeroe, getHeroeById, getHeroes} from "../controllers/heroeController";
+import {createHeroe, getHeroeById, getHeroes, updateHeroeById} from "../controllers/heroeController";
 
 const heroeRouter = Router();
 
 heroeRouter.get('/', getHeroes);
 heroeRouter.get('/:id', validateHeroe(heroeIdSchema, 'params'), getHeroeById);
 heroeRouter.post('/', validateHeroe(heroeCreateSchema, 'body'), createHeroe);
+// quiero utilizar queryString --> aunque sea poco común
+heroeRouter.put('/', validateHeroe(heroeIdSchema, 'query'), validateHeroe(heroeCreateSchema, 'body'), updateHeroeById);
 
 export default heroeRouter;
