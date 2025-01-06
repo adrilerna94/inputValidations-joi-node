3456e4fe7249e03497526cd85e09851cc6bd7d00
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-06 21:25:37 +0100

    feat: :white_check_mark: getHeroeById done

--- src/api/controllers/heroeController.ts
+++ src/api/controllers/heroeController.ts
@@ -3,4 +3,18 @@ import type { Request, Response, } from "express";
 
 const getHeroes = (req: Request, res: Response) => res.send(heroes);
 
-export {getHeroes};
+const getHeroeById = (req: Request, res: Response) => {
+  const id = req.params.id;
+  const heroe = heroes.find((heroe) => heroe.id === id);
+
+  if (!heroe){
+    res.status(404).send({ERROR: `Heroe with ID = ${id} Not Found.`});
+  }
+  const identifier:string = heroe?.alias || heroe?.email || 'default identifier';
+  res.status(200).json({
+    status: 'Success',
+    [identifier]: heroe
+  });
+}
+
+export {getHeroes, getHeroeById};
