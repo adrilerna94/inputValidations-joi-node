59ab391bc7dab5c4fb403fe713d317efe7d0f1e9
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-06 23:10:28 +0100

    feat: :white_check_mark: create Heroe done

--- src/api/controllers/heroeController.ts
+++ src/api/controllers/heroeController.ts
@@ -1,5 +1,6 @@
 import { heroes } from "../data/heroes";
-import type { Request, Response, } from "express";
+import { v4 as uuidv4} from "uuid";
+import type { Request, Response } from "express";
 
 const getHeroes = (req: Request, res: Response) => res.send(heroes);
 
@@ -8,13 +9,35 @@ const getHeroeById = (req: Request, res: Response) => {
   const heroe = heroes.find((heroe) => heroe.id === id);
 
   if (!heroe){
-    res.status(404).send({ERROR: `Heroe with ID = ${id} Not Found.`});
+    return res.status(404).send({ERROR: `Heroe with ID = ${id} Not Found.`});
   }
   const identifier:string = heroe?.alias || heroe?.email || 'default identifier';
-  res.status(200).json({
+  return res.status(200).json({
     status: 'Success',
     [identifier]: heroe
   });
 }
 
-export {getHeroes, getHeroeById};
+function createHeroe (req: Request, res: Response) {
+
+  // generamos Universal Unique Identifier version 4 para el ID de forma automática con la librería uuid
+  const newHeroe = {id: uuidv4(), ...req.body};
+  const {id} = newHeroe;
+
+  try {
+    heroes.push(newHeroe);
+    const identifier : string = newHeroe.alias || newHeroe.id;
+    return res.status(201).send({
+      msn: `Heroe with ID ${id} created Successfully.`,
+      [identifier]: newHeroe
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msn : `Failed to add Heroe with ID ${id}.`,
+      error: error.message
+    });
+  }
+
+}
+
+export {getHeroes, getHeroeById, createHeroe};
