b3da8eb24286e02dadfde29ca30b5a83346672d1
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-01-07 01:21:54 +0100

    feat: :white_check_mark: delete route done

--- src/api/routers/heroeRouter.ts
+++ src/api/routers/heroeRouter.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { validateHeroe } from "../middlewares/heroeValidation";
 import { heroeIdSchema, heroeCreateSchema } from "../models/Joi/heroeSchemas";
-import {createHeroe, getHeroeById, getHeroes, updateHeroeById} from "../controllers/heroeController";
+import {createHeroe, deleteHeroeById, getHeroeById, getHeroes, updateHeroeById} from "../controllers/heroeController";
 
 const heroeRouter = Router();
 
@@ -10,5 +10,6 @@ heroeRouter.get('/:id', validateHeroe(heroeIdSchema, 'params'), getHeroeById);
 heroeRouter.post('/', validateHeroe(heroeCreateSchema, 'body'), createHeroe);
 // quiero utilizar queryString --> aunque sea poco común
 heroeRouter.put('/', validateHeroe(heroeIdSchema, 'query'), validateHeroe(heroeCreateSchema, 'body'), updateHeroeById);
+heroeRouter.delete('/:id', validateHeroe(heroeIdSchema, 'params'), deleteHeroeById);
 
 export default heroeRouter;
